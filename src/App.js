37e8a849bbb66cc52c75f2c374aeca46fbb25fac
amit <amitperane8473@gmail.com>
2021-06-05 14:49:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios'
+export default class App extends Component {
+  state = {
+    advices: '',
+    // loading : true
+  }
+  componentDidMount() {
+    this.fetchAdvice()
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  }
+  fetchAdvice = () => {
+
+    axios.get('https://api.adviceslip.com/advice')
+      .then((res) => {
+        const { advice } = res.data.slip
+        console.log(advice)
+        this.setState({
+          //  loading : false,
+          advices: advice
+        })
+        // console.log(res.data.slip.advice);
+      })
+      .catch((err) => {
+        console.log("Error", err)
+      })
+  }
+  render() {
+    const mystyle = {
+      width: ' 50%',
+      height: 'auto',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: 'red',
+    }
+    const AppStyle = {
+      display: "flex",
+      alignItems: 'center',
+      justifyContent: 'center',
+      height: '100vh',
+    }
+    const { advices } = this.state
+    return (
+      <div className="App" style={AppStyle} >
+        <div style={mystyle} >
+          <div style={{ padding: '20px', textAlign: "center" }} >
+            <p>{advices}</p>
+            {/* <p {...this.state.loading ? true : '<p>loading<p/>' }></p> */}
+          </div>
+          <div>
+            <button onClick={this.fetchAdvice} style={{ marginBottom: '20px' }}>Next</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+
