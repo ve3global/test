44662d99206590ac3d328b34909b835981a974a4
Ramprasad-ve3 <126949884+Ramprasad-ve3@users.noreply.github.com>
2023-03-09 21:44:17 +0530

    Update create-pipeline.sh

--- create-pipeline.sh
+++ create-pipeline.sh
@@ -17,4 +17,4 @@ aws cloudformation create-stack \
         --capabilities CAPABILITY_IAM \
         --stack-name $CODEPIPELINE_STACK_NAME \
         --parameters ParameterKey=GitHubOAuthToken,ParameterValue=${1} \
-        --template-body file://pipeline.yaml
+        --template-body file://test.yaml
